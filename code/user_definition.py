8a20d6f2c71755c126062034207ee673a6c339d3
areevesman <areevesman@gmail.com>
2020-04-13 17:21:18 -0700

    deployment

---
+++ code/user_definition.py
@@ -0,0 +1,9 @@
+# ec2
+ec2_address = "ec2-54-202-188-168.us-west-2.compute.amazonaws.com"
+user = "ec2-user"
+key_file = "/aws_credentials/covid-19.pem"
+
+# github repo
+git_user_id = "areevesman"
+git_repo_owner = "areevesman"
+git_repo_name = "covid-19"
